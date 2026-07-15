5d6ad4ed0f73b95fc1b70959ce672b4936d0b8cc
JosueSENE <josueguilay@gmail.com>
2026-07-15 19:57:28 +0000

    Modification de la base de donnée et implemantation de VoteDAO

--- src/app/dao/UserDAO.java
+++ src/app/dao/UserDAO.java
@@ -45,6 +45,7 @@ public class UserDAO {
         return null; // Échec de l'authentification (mauvais login ou mauvais mot de passe)
     }
 
+    // ===================== CRUD  ==============================
 
     // AJOUTER UN UTILISATUR (CREATE)
 
@@ -78,21 +79,34 @@ public class UserDAO {
         }
     }
 
+    //RECHERCHER UN UTILISATEUR PAR SON email (READ)
 
-    //SUPPRIMER UN UTILISATEUR
-
-    public void deleteUser(int email) {
-        String sql = "DELETE FROM users WHERE email = ?";
-        try (Connection conn = DBConnection.getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql)) {
-            ps.setInt(1, email);
-            if (ps.executeUpdate() ==  0) throw new SQLException("Echec lors de la suppression");
-            else System.err.println("Succes: Utilisateur supprimer dans la base de données ");
-        } catch (SQLException e) {
+    public User searchUser (String email){
+        String sql = "SELECT * FROM users WHERE email= ?";
+        try  (Connection conn = DBConnection.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql)){  
+            ps.setString(1, email);
+            try (ResultSet rs = ps.executeQuery()){
+                User user = new User();
+                user.setId(rs.getInt("id"));
+                user.setCode_permanent(rs.getInt("code_permanent"));
+                user.setNom(rs.getString("nom"));
+                user.setPrenom(rs.getString("prenom"));
+                user.setEmail(rs.getString("email"));
+                user.setRole(rs.getString("role"));
+                // Récupération des clés étrangères optionnelles
+                int filiereId = rs.getInt("filiere_id");
+                user.setFiliere_id(rs.wasNull() ? null : filiereId);
+                user.setNiveau(rs.getString("niveau"));                        
+                return user; // Succès
+                }          
+            } catch (SQLException e) {
             e.printStackTrace();
         }
+        return null;
     }
-    //RECUPERER TOUS LES UTILISATEURS
+
+    //RECUPERER TOUS LES UTILISATEURS (READ)
 
     public List<User> getAllUsers(){
         List<User> liste = new ArrayList<>();
@@ -117,6 +131,24 @@ public class UserDAO {
             e.printStackTrace();
         }
         return liste;
+    }
+
+    //MODIFIER UN UTILISATEUR (UPDATE)
+
+
+
+    //SUPPRIMER UN UTILISATEUR (DELETE)
+
+    public void deleteUser(int email) {
+        String sql = "DELETE FROM users WHERE email = ?";
+        try (Connection conn = DBConnection.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, email);
+            if (ps.executeUpdate() ==  0) throw new SQLException("Echec lors de la suppression");
+            else System.err.println("Succes: Utilisateur supprimer dans la base de données ");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
     
 }
